1ea5fcf22642cc0060f0a98c8e48d535ed1a7e4b
DeaneKane <deank06@hotmail.com>
2020-02-04 14:14:01 +0000

    teams functionality added

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,38 +14,52 @@ import selenium.Login;
 @SuppressWarnings("restriction")
 public class Main extends Application
 {
+
+    List<Button> teamNamesButtons = new ArrayList<>();
+
+
     public static void main( String[] args )
-    {   
-        launch(args);
+    {
+        launch( args );
     }
+
 
     @Override
     public void start( Stage primaryStage ) throws Exception
     {
         Scene mainScene;
         Button loginButton = new Button( "Login" );
+        VBox layout1 = new VBox( 20 );
         loginButton.setOnAction( e -> {
             Login login = new Login();
             login.loginUser();
             loginButton.setText( login.getUser() );
-            loginButton.setDisable(true);            
+            loginButton.setDisable( true );
         } );
-        
+
         Button getTeamsButton = new Button( "Load Teams and Details" );
         getTeamsButton.setOnAction( e -> {
             GetTeams getTeams = new GetTeams();
             getTeams.scrapeTeams();
-            
-        });
-        
+            if( !getTeams.getTeams().isEmpty() )
+            {
+                for( int i = 0; i < getTeams.getTeams().size(); i++ )
+                {
+                    Button teamName = new Button( getTeams.getTeams().get( i ) );
+                    teamNamesButtons.add( teamName );
+                    layout1.getChildren().add( teamNamesButtons.get( i ) );
+                }
+                getTeamsButton.setDisable( true );
+            }
+        } );
+
         // Layout 1
-        VBox layout1 = new VBox( 20 );
-        layout1.getChildren().addAll(  loginButton, getTeamsButton );
+        layout1.getChildren().addAll( loginButton, getTeamsButton);
         mainScene = new Scene( layout1, 200, 200 );
 
         primaryStage.setScene( mainScene );
         primaryStage.setTitle( "GB Getter" );
         primaryStage.show();
-        
+
     }
 }
--- src/selenium/GetTeams.java
+++ src/selenium/GetTeams.java
@@ -2,63 +2,34 @@ package selenium;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 public class GetTeams extends StartFirefoxDriver
 {
     ArrayList<String> teams = new ArrayList<>();
 
+    List<WebElement> teamElements;
+
+    //Constructer
     public GetTeams()
     {
-
+        //Constructer
     }
 
 
-    public ArrayList<String> scrapeTeams(){
-        
-        Actions action = new Actions(driver);
-        WebElement teamsHover = driver.findElement( By.xpath( "/html/body/gb-root/gb-main-layout/div/div/sh-header-container/sh-header/div/div/div[2]/sh-submenu-trigger[1]/div/button/sh-teams-icon/img" ) );
-        action.moveToElement( teamsHover );
-        boolean mt;
-        int i = 1;
-        do {           
-                        
-            mt = moreTeams(i);
-            System.out.println(driver.findElement( By.xpath( "/html/body/gb-root/gb-main-layout/div/div/sh-header-container/sh-header/div/div/div[2]/sh-submenu-trigger[1]/div/div/sh-header-sub-menu/div/div/div/sh-teams-component/div/a["
-                    + i
-                    + "]/div/div[1]")).getText());
-            teams.add( driver.findElement( By.xpath( "/html/body/gb-root/gb-main-layout/div/div/sh-header-container/sh-header/div/div/div[2]/sh-submenu-trigger[1]/div/div/sh-header-sub-menu/div/div/div/sh-teams-component/div/a["
-                                          + i
-                                          + "]/div/div[1]" )).getText());
-            i++;
-
-                                  
-        } while(mt);
-        
-        
+    public List<String> scrapeTeams(){
+                
+        driver.navigate().to("https://gamebattles.majorleaguegaming.com/my-competitions");
+        teamElements = driver.findElementsByClassName( "team-name" );        
+        for(WebElement languageElement : teamElements) {
+            teams.add( languageElement.getText() );           
+        }
+               
         return teams;
-        
     }
 
-
-    public boolean moreTeams( int i )
+    public List<String> getTeams()
     {
-
-        if( driver.findElement( By.xpath( "/html/body/gb-root/gb-main-layout/div/div/sh-header-container/sh-header/div/div/div[2]/sh-submenu-trigger[1]/div/div/sh-header-sub-menu/div/div/div/sh-teams-component/div/a["
-                                          + i
-                                          + "]/div/div[1]" ) ) != null )
-        {
-            
-            return true;
-        }
-        else
-        {
-
-            return false;
-        }
+        return teams;
     }
-
 }
