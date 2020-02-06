54c352c416fa03a3bdc725382d621378f49c11f9
DeaneKane <deank06@hotmail.com>
2020-02-06 16:38:19 +0000

    stale element exception handling, still needs sorted

--- src/selenium/GetTeams.java
+++ src/selenium/GetTeams.java
@@ -13,7 +13,6 @@ public class GetTeams extends StartFirefoxDriver
     ArrayList<String> teams = new ArrayList<>();
     ArrayList<String> roster = new ArrayList<>();
     ArrayList<String> teamIds = new ArrayList<>();
-    List<WebElement> teamElements;
     List<WebElement> rosterMembers;
 
 
@@ -28,11 +27,18 @@ public class GetTeams extends StartFirefoxDriver
     {
 
         driver.navigate().to( "https://gamebattles.majorleaguegaming.com/my-competitions" );
-        teamElements = driver.findElementsByClassName( "team-name" );
-        for( WebElement languageElement : teamElements )
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        List<WebElement> teamElements = driver.findElementsByClassName( "team-name" );
+        int size = teamElements.size();
+        
+        for( int i = 0; i < size; i++ )
         {
-            teams.add( languageElement.getText() );
-            scrapeEachTeamDetails( languageElement );
+            List<WebElement> teamElementsInLoop = driver.findElementsByClassName( "team-name" );
+            wait.until(ExpectedConditions.elementToBeClickable((By.className("team-name"))));
+            System.out.println(teamElementsInLoop.get( 1 ).getText());
+            System.out.println(teamElementsInLoop.get( i ).getText());
+            teams.add( teamElementsInLoop.get(i).getText() );
+            scrapeEachTeamDetails( teamElementsInLoop.get( i ) );
         }
 
         return teams;
@@ -43,14 +49,18 @@ public class GetTeams extends StartFirefoxDriver
     {
 
         langElement.click();
-        new WebDriverWait(driver, 20).until(ExpectedConditions.urlContains("team"));
+        new WebDriverWait( driver, 20 ).until( ExpectedConditions.urlContains( "team" ) );
         String currentUrl = driver.getCurrentUrl();
-        teamIds.add( currentUrl.substring( 69 ));
+        teamIds.add( currentUrl.substring( 69 ) );
+        System.out.println( currentUrl.substring( 69 ) );
         rosterMembers = driver.findElementsByClassName( "member-username" );
         for( WebElement languageElement : rosterMembers )
         {
             roster.add( languageElement.getText() );
+            System.out.println( languageElement.getText() );
+
         }
+        driver.navigate().back();
 
     }
 
